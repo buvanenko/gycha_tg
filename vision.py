5945808a45f40297059725e821a59554f4df7724
buvanenko <gleb@buvanenko.ru>
2025-02-17 15:49:26 +0300

    moondream2

--- vision.py
+++ vision.py
@@ -1,21 +1,14 @@
 import aiohttp
 
-import moondream as md
-from io import BytesIO
-from PIL import Image
-
-model = md.vl(model="moondream-0_5b-int8.mf.gz")
+from ollama import AsyncClient
 
 async def get(url: str):
-
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             content = await response.read()
 
-    img = Image.open(BytesIO(content))
-    encoded_image = model.encode_image(img)
+    # img = BytesIO(content)
 
-    caption = model.caption(encoded_image)["caption"]
-    print(caption)
-
-    return caption
+    caption = await AsyncClient().generate(model="moondream", prompt="What is this?", images=[content])
+    print(caption.response)
+    return caption.response
